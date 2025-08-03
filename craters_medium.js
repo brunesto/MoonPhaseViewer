d5b70c76a05659c587ebff09553357dedcae36d2
brunesto <bruno.carle@gmail.com>
2025-08-03 17:32:34 +0200

    use FullMoon2010 as base image

---
+++ craters_medium.js
@@ -0,0 +1,520 @@
+//list of 86 near side craters with diameter [100,300] km
+//extracted from wikipedia. Text is available under the Creative Commons Attribution-ShareAlike 4.0 License; additional terms may apply. 
+craters_medium=[
+    {
+        "name": "Abel",
+        "diameter": 137.35,
+        "lat": -34.63333333333333,
+        "lon": 85.78333333333333
+    },
+    {
+        "name": "Albategnius",
+        "diameter": 130.84,
+        "lat": -11.233333333333333,
+        "lon": 4.016666666666667
+    },
+    {
+        "name": "Alphonsus",
+        "diameter": 110.54,
+        "lat": -13.383333333333333,
+        "lon": -2.85
+    },
+    {
+        "name": "Amundsen",
+        "diameter": 103.39,
+        "lat": -84.43333333333334,
+        "lon": 83.06666666666666
+    },
+    {
+        "name": "Babbage",
+        "diameter": 146.56,
+        "lat": 59.56666666666667,
+        "lon": -57.38333333333333
+    },
+    {
+        "name": "Balmer",
+        "diameter": 136.3,
+        "lat": -20.266666666666666,
+        "lon": 70.21666666666667
+    },
+    {
+        "name": "Barnard",
+        "diameter": 115.73,
+        "lat": -29.783333333333335,
+        "lon": 85.95
+    },
+    {
+        "name": "Blancanus",
+        "diameter": 105.82,
+        "lat": -63.766666666666666,
+        "lon": -21.633333333333333
+    },
+    {
+        "name": "Boussingault",
+        "diameter": 127.61,
+        "lat": -70.21666666666667,
+        "lon": 53.733333333333334
+    },
+    {
+        "name": "Brianchon",
+        "diameter": 137.26,
+        "lat": 74.75,
+        "lon": -88.36666666666666
+    },
+    {
+        "name": "Cabeus",
+        "diameter": 100.58,
+        "lat": -85.33333333333333,
+        "lon": -42.13333333333333
+    },
+    {
+        "name": "Casatus",
+        "diameter": 102.85,
+        "lat": -72.7,
+        "lon": -30.75
+    },
+    {
+        "name": "Clavius",
+        "diameter": 230.77,
+        "lat": -58.61666666666667,
+        "lon": -14.733333333333333
+    },
+    {
+        "name": "Cleomedes",
+        "diameter": 130.77,
+        "lat": 27.6,
+        "lon": 55.5
+    },
+    {
+        "name": "Darwin",
+        "diameter": 122.18,
+        "lat": -19.933333333333334,
+        "lon": -69.21666666666667
+    },
+    {
+        "name": "De La Rue",
+        "diameter": 135.22,
+        "lat": 59.016666666666666,
+        "lon": 52.833333333333336
+    },
+    {
+        "name": "Demonax",
+        "diameter": 121.93,
+        "lat": -78.08333333333333,
+        "lon": 59.36666666666667
+    },
+    {
+        "name": "Deslandres",
+        "diameter": 227.02,
+        "lat": -32.55,
+        "lon": -5.566666666666666
+    },
+    {
+        "name": "Drygalski",
+        "diameter": 162.49,
+        "lat": -79.56666666666666,
+        "lon": -87.18333333333334
+    },
+    {
+        "name": "Eddington",
+        "diameter": 120.13,
+        "lat": 21.5,
+        "lon": -72.01666666666667
+    },
+    {
+        "name": "Einstein",
+        "diameter": 181.47,
+        "lat": 16.6,
+        "lon": -88.65
+    },
+    {
+        "name": "Endymion",
+        "diameter": 122.1,
+        "lat": 53.61666666666667,
+        "lon": 56.483333333333334
+    },
+    {
+        "name": "Fracastorius",
+        "diameter": 120.58,
+        "lat": -21.366666666666667,
+        "lon": 33.06666666666667
+    },
+    {
+        "name": "Furnerius",
+        "diameter": 135.03,
+        "lat": -36.0,
+        "lon": 60.53333333333333
+    },
+    {
+        "name": "Gärtner",
+        "diameter": 101.79,
+        "lat": 59.233333333333334,
+        "lon": 34.766666666666666
+    },
+    {
+        "name": "Gassendi",
+        "diameter": 111.39,
+        "lat": -17.55,
+        "lon": -39.96666666666667
+    },
+    {
+        "name": "Gauss",
+        "diameter": 170.72,
+        "lat": 36.016666666666666,
+        "lon": 79.08333333333333
+    },
+    {
+        "name": "Gilbert",
+        "diameter": 100.25,
+        "lat": -3.2,
+        "lon": 76.16666666666667
+    },
+    {
+        "name": "Goldschmidt",
+        "diameter": 115.26,
+        "lat": 73.03333333333333,
+        "lon": -3.3666666666666667
+    },
+    {
+        "name": "Grimaldi",
+        "diameter": 173.49,
+        "lat": -5.383333333333334,
+        "lon": -68.36666666666666
+    },
+    {
+        "name": "Hausen",
+        "diameter": 163.24,
+        "lat": -65.11666666666666,
+        "lon": -88.48333333333333
+    },
+    {
+        "name": "Hecataeus",
+        "diameter": 133.67,
+        "lat": -22.066666666666666,
+        "lon": 79.68333333333334
+    },
+    {
+        "name": "Hedin",
+        "diameter": 157.38,
+        "lat": 2.8666666666666667,
+        "lon": -76.56666666666666
+    },
+    {
+        "name": "Helmholtz",
+        "diameter": 110.16,
+        "lat": -68.63333333333334,
+        "lon": 65.33333333333333
+    },
+    {
+        "name": "Hevelius",
+        "diameter": 113.87,
+        "lat": 2.2,
+        "lon": -67.46666666666667
+    },
+    {
+        "name": "Hipparchus",
+        "diameter": 143.95,
+        "lat": -5.366666666666666,
+        "lon": 4.916666666666667
+    },
+    {
+        "name": "Hommel",
+        "diameter": 113.6,
+        "lat": -54.733333333333334,
+        "lon": 32.93333333333333
+    },
+    {
+        "name": "Humboldt",
+        "diameter": 199.46,
+        "lat": -27.016666666666666,
+        "lon": 80.96666666666667
+    },
+    {
+        "name": "J. Herschel",
+        "diameter": 154.44,
+        "lat": 62.31666666666667,
+        "lon": -41.86666666666667
+    },
+    {
+        "name": "Janssen",
+        "diameter": 200.65,
+        "lat": -44.96666666666667,
+        "lon": 40.81666666666667
+    },
+    {
+        "name": "Kästner",
+        "diameter": 116.07,
+        "lat": -6.9,
+        "lon": 78.93333333333334
+    },
+    {
+        "name": "Klaproth",
+        "diameter": 121.37,
+        "lat": -69.85,
+        "lon": -26.266666666666666
+    },
+    {
+        "name": "Lagrange",
+        "diameter": 162.21,
+        "lat": -32.6,
+        "lon": -71.45
+    },
+    {
+        "name": "Lamarck",
+        "diameter": 114.65,
+        "lat": -23.116666666666667,
+        "lon": -70.06666666666666
+    },
+    {
+        "name": "Langrenus",
+        "diameter": 131.98,
+        "lat": -8.866666666666667,
+        "lon": 61.03333333333333
+    },
+    {
+        "name": "Le Gentil",
+        "diameter": 125.38,
+        "lat": -74.26666666666667,
+        "lon": -76.01666666666667
+    },
+    {
+        "name": "Letronne",
+        "diameter": 117.59,
+        "lat": -10.5,
+        "lon": -42.483333333333334
+    },
+    {
+        "name": "Longomontanus",
+        "diameter": 145.5,
+        "lat": -49.55,
+        "lon": -21.883333333333333
+    },
+    {
+        "name": "Lyot",
+        "diameter": 150.6,
+        "lat": -50.46666666666667,
+        "lon": 84.8
+    },
+    {
+        "name": "Maginus",
+        "diameter": 155.58,
+        "lat": -50.03333333333333,
+        "lon": -5.983333333333333
+    },
+    {
+        "name": "Maurolycus",
+        "diameter": 115.35,
+        "lat": -41.766666666666666,
+        "lon": 13.916666666666666
+    },
+    {
+        "name": "Mee",
+        "diameter": 134.11,
+        "lat": -43.63333333333333,
+        "lon": -35.18333333333333
+    },
+    {
+        "name": "Messala",
+        "diameter": 122.4,
+        "lat": 39.31666666666667,
+        "lon": 60.06666666666667
+    },
+    {
+        "name": "Meton",
+        "diameter": 124.7,
+        "lat": 73.56666666666666,
+        "lon": 19.633333333333333
+    },
+    {
+        "name": "Moretus",
+        "diameter": 114.45,
+        "lat": -70.63333333333334,
+        "lon": -5.95
+    },
+    {
+        "name": "Neper",
+        "diameter": 144.32,
+        "lat": 8.766666666666667,
+        "lon": 84.58333333333333
+    },
+    {
+        "name": "Orontius",
+        "diameter": 121.02,
+        "lat": -40.36666666666667,
+        "lon": -3.966666666666667
+    },
+    {
+        "name": "Pascal",
+        "diameter": 108.2,
+        "lat": 74.36666666666666,
+        "lon": -70.63333333333334
+    },
+    {
+        "name": "Petavius",
+        "diameter": 184.06,
+        "lat": -25.383333333333333,
+        "lon": 60.78333333333333
+    },
+    {
+        "name": "Phillips",
+        "diameter": 104.21,
+        "lat": -26.566666666666666,
+        "lon": 75.66666666666667
+    },
+    {
+        "name": "Phocylides",
+        "diameter": 115.18,
+        "lat": -52.78333333333333,
+        "lon": -57.31666666666667
+    },
+    {
+        "name": "Piazzi",
+        "diameter": 102.57,
+        "lat": -36.166666666666664,
+        "lon": -68.01666666666667
+    },
+    {
+        "name": "Pitatus",
+        "diameter": 100.63,
+        "lat": -29.883333333333333,
+        "lon": -13.533333333333333
+    },
+    {
+        "name": "Plato",
+        "diameter": 100.68,
+        "lat": 51.61666666666667,
+        "lon": -9.383333333333333
+    },
+    {
+        "name": "Ptolemaeus",
+        "diameter": 153.67,
+        "lat": -9.166666666666666,
+        "lon": -1.8333333333333335
+    },
+    {
+        "name": "Purbach",
+        "diameter": 114.97,
+        "lat": -25.516666666666666,
+        "lon": -2.033333333333333
+    },
+    {
+        "name": "Pythagoras",
+        "diameter": 144.55,
+        "lat": 63.68333333333333,
+        "lon": -62.983333333333334
+    },
+    {
+        "name": "Rayleigh",
+        "diameter": 113.77,
+        "lat": 29.116666666666667,
+        "lon": 89.45
+    },
+    {
+        "name": "Regiomontanus",
+        "diameter": 126.64,
+        "lat": -28.283333333333335,
+        "lon": -1.0833333333333333
+    },
+    {
+        "name": "Repsold",
+        "diameter": 108.65,
+        "lat": 51.31666666666667,
+        "lon": -78.41666666666667
+    },
+    {
+        "name": "Riccioli",
+        "diameter": 155.66,
+        "lat": -2.9,
+        "lon": -74.41666666666667
+    },
+    {
+        "name": "Riemann",
+        "diameter": 117.85,
+        "lat": 39.38333333333333,
+        "lon": 87.18333333333334
+    },
+    {
+        "name": "Russell",
+        "diameter": 103.37,
+        "lat": 26.516666666666666,
+        "lon": -75.55
+    },
+    {
+        "name": "Scheiner",
+        "diameter": 110.07,
+        "lat": -60.35,
+        "lon": -27.816666666666666
+    },
+    {
+        "name": "Schickard",
+        "diameter": 212.18,
+        "lat": -44.38333333333333,
+        "lon": -55.11666666666667
+    },
+    {
+        "name": "Schiller",
+        "diameter": 179.36,
+        "lat": -51.71666666666667,
+        "lon": -39.78333333333333
+    },
+    {
+        "name": "Scott",
+        "diameter": 107.82,
+        "lat": -82.35,
+        "lon": 48.516666666666666
+    },
+    {
+        "name": "South",
+        "diameter": 119.04,
+        "lat": 57.583333333333336,
+        "lon": -50.93333333333333
+    },
+    {
+        "name": "Stöfler",
+        "diameter": 129.87,
+        "lat": -41.233333333333334,
+        "lon": 5.933333333333334
+    },
+    {
+        "name": "Struve",
+        "diameter": 164.34,
+        "lat": 23.416666666666668,
+        "lon": -76.65
+    },
+    {
+        "name": "Vendelinus",
+        "diameter": 141.21,
+        "lat": -16.466666666666665,
+        "lon": 61.55
+    },
+    {
+        "name": "Volta",
+        "diameter": 117.15,
+        "lat": 53.9,
+        "lon": -84.76666666666667
+    },
+    {
+        "name": "W. Bond",
+        "diameter": 170.53,
+        "lat": 65.41666666666667,
+        "lon": 3.5166666666666666
+    },
+    {
+        "name": "Walther",
+        "diameter": 134.23,
+        "lat": -33.25,
+        "lon": 0.6166666666666667
+    },
+    {
+        "name": "Wilhelm",
+        "diameter": 100.83,
+        "lat": -43.21666666666667,
+        "lon": -20.933333333333334
+    },
+    {
+        "name": "Xenophanes",
+        "diameter": 117.57,
+        "lat": 57.483333333333334,
+        "lon": -82.01666666666667
+    }
+]
---
+++ mares.js
@@ -0,0 +1,144 @@
+
+
+const mares =
+[
+    {
+        "name": "Mare Anguis",
+        "lat": 22.6,
+        "lon": 67.7,
+        "diameter": 150
+    },
+    {
+        "name": "Mare Australe",
+        "lat": -38.9,
+        "lon": 93.0,
+        "diameter": 603
+    },
+    {
+        "name": "Mare Cognitum",
+        "lat": -10.0,
+        "lon": -23.1,
+        "diameter": 376
+    },
+    {
+        "name": "Mare Crisium",
+        "lat": 17.0,
+        "lon": 59.1,
+        "diameter": 418
+    },
+    {
+        "name": "Mare Fecunditatis",
+        "lat": -7.8,
+        "lon": 51.3,
+        "diameter": 909
+    },
+    {
+        "name": "Mare Frigoris",
+        "lat": 56.0,
+        "lon": 1.4,
+        "diameter": 1596
+    },
+    {
+        "name": "Mare Humboldtianum",
+        "lat": 56.8,
+        "lon": 81.5,
+        "diameter": 273
+    },
+    {
+        "name": "Mare Humorum",
+        "lat": -24.4,
+        "lon": -38.6,
+        "diameter": 389
+    },
+    {
+        "name": "Mare Imbrium",
+        "lat": 32.8,
+        "lon": -15.6,
+        "diameter": 1123
+    },
+    {
+        "name": "Mare Ingenii",
+        "lat": -33.7,
+        "lon": 163.5,
+        "diameter": 318
+    },
+    {
+        "name": "Mare Insularum",
+        "lat": 7.5,
+        "lon": -30.9,
+        "diameter": 513
+    },
+    {
+        "name": "Mare Marginis",
+        "lat": 13.3,
+        "lon": 86.1,
+        "diameter": 420
+    },
+    {
+        "name": "Mare Moscoviense",
+        "lat": 27.3,
+        "lon": 147.9,
+        "diameter": 277
+    },
+    {
+        "name": "Mare Nectaris",
+        "lat": -15.2,
+        "lon": 35.5,
+        "diameter": 333
+    },
+    {
+        "name": "Mare Nubium",
+        "lat": -21.3,
+        "lon": -16.6,
+        "diameter": 715
+    },
+    {
+        "name": "Mare Orientale",
+        "lat": -19.4,
+        "lon": -92.8,
+        "diameter": 327
+    },
+    {
+        "name": "Mare Serenitatis",
+        "lat": 28.0,
+        "lon": 17.5,
+        "diameter": 707
+    },
+    {
+        "name": "Mare Smythii",
+        "lat": 1.3,
+        "lon": 87.5,
+        "diameter": 373
+    },
+    {
+        "name": "Mare Spumans",
+        "lat": 1.1,
+        "lon": 65.1,
+        "diameter": 139
+    },
+    {
+        "name": "Mare Tranquillitatis",
+        "lat": 8.5,
+        "lon": 31.4,
+        "diameter": 873
+    },
+    {
+        "name": "Mare Undarum",
+        "lat": 6.8,
+        "lon": 68.4,
+        "diameter": 243
+    },
+    {
+        "name": "Mare Vaporum",
+        "lat": 13.3,
+        "lon": 3.6,
+        "diameter": 245
+    },
+    {
+        "name": "Oceanus Procellarum",
+        "lat": 18.4,
+        "lon": -57.4,
+        "diameter": 2568
+    }
+]
+;
