7df9f935a4a55a64848723df19a8442b9d6d48fe
brunesto <bruno.carle@gmail.com>
2025-08-07 01:51:27 +0200

    landings

---
+++ landings.js
@@ -0,0 +1,43 @@
+// from https://nssdc.gsfc.nasa.gov/planetary/lunar/lunar_sites.html
+landings=[
+    {
+        "name": "Apollo 11",        
+        "lat":0.67416 	,
+        "lon": 23.47314,
+        "link": "https://nssdc.gsfc.nasa.gov/nmc/spacecraft/display.action?id=1969-059C"
+    },
+    
+    {
+        "name": "Apollo 12",        
+        "lat":-3.0128 ,
+        "lon":-23.4219,
+        "link": "http://nssdc.gsfc.nasa.gov/nmc/spacecraft/display.action?id=1969-099C"
+    },
+    
+    {
+        "name": "Apollo 14",        
+        "lat":-3.64589  ,
+        "lon":	-17.47194,
+        "link": "https://nssdc.gsfc.nasa.gov/nmc/spacecraft/display.action?id=1971-008C"
+    },
+
+    {
+        "name": "Apollo 15",        
+        "lat":26.13239 	 ,
+        "lon":	3.63330,
+        "link": "http://nssdc.gsfc.nasa.gov/nmc/spacecraft/display.action?id=1971-063C"
+    },
+    {
+        "name": "Apollo 16",        
+        "lat":-8.9734 	 ,
+        "lon":15.5011,
+        "link": "http://nssdc.gsfc.nasa.gov/nmc/spacecraft/display.action?id=1972-031C"
+    },
+    {
+        "name": "Apollo 17",        
+        "lat":20.1911 	 ,
+        "lon":	30.7723,
+        "link": "http://nssdc.gsfc.nasa.gov/nmc/spacecraft/display.action?id=1972-096C"
+    },
+    
+]
